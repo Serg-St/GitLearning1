1a8437239bc3417ad72bafb54febcea744ca72f4
Sergey <honasfred@gmail.com>
2020-10-12 21:17:57 +0700

    Добавлен цикл инициализации списка

--- GitLearning1/Program.cs
+++ GitLearning1/Program.cs
@@ -9,12 +9,10 @@ namespace GitLearning1
 	{
 		static void Main(string[] args)
 		{
-			// The code provided will print ‘Hello World’ to the console.
-			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-			Console.WriteLine("Hello World!");
-			Console.ReadKey();
-
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+			var len = 50;
+			var list = new List<int>();
+			for (var i = 0; i < len; i++)
+				list.Add(i);
 		}
 	}
 }
